a750a7aa563375f71c4e243fc7ca6ef4ec49fd1d
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-04-13 23:06:26 +0200

    JS clear

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,6 @@ const rollQuote = () => {
     fetch(`https://api.adviceslip.com/advice`)
 	.then((res) => res.json())
     .then(data => {
-        console.log(data.slip);
         const adviceRes = data.slip.advice
         const idRes = data.slip.id
         quote.textContent = adviceRes
@@ -15,11 +14,3 @@ const rollQuote = () => {
 }
 
 diceBtn.addEventListener('click', rollQuote)
-
-// fetch(URL)
-// 	.then((res) => res.json())
-// 	.then((res) => one.setAttribute('src', res.message))
-
-// axios.get(URL).then((res) => {
-//         two.setAttribute('src', res.data.message)
-// })

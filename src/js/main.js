2183b153a73aaf17274ca9d1a6ccb04d478232d0
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-04-14 22:58:47 +0200

    JS fix for Mozilla-firefox
    There is a problem in Mozilla-firefox, without adding cache:no-store in fetch() after url, it wont reload quotes, so it must be added in that case

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ const quote = document.querySelector('.advice')
 const diceBtn = document.querySelector('.generator__btn')
 
 const rollQuote = () => {
-    fetch(`https://api.adviceslip.com/advice`)
+    fetch(`https://api.adviceslip.com/advice`, {
+        cache: 'no-store'
+        //without no-store there is a problem in Mozilla Firefox - api won't reload quotes
+    })
 	.then((res) => res.json())
     .then(data => {
         const adviceRes = data.slip.advice
